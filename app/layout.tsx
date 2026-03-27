a216da339fe61eb77eca682eea2a8cd746bb5b7e
Jamiul Islam <jamiulislamjami@hotmail.com>
2026-03-27 17:06:48 +0600

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import FooterSection from "@/components/sections/FooterSection";
+import { Noto_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const notoSans = Noto_Sans({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "iTellFortune",
@@ -13,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", notoSans.variable)}>
       <body>
         {children}
         <FooterSection />
